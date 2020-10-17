4a3f4818e7b662725990516f8c2b0e9470255d2d
Veronika <skripko.veronika@yandex.ru>
2020-10-17 20:06:58 +0300

    OOP

---
+++ lesson5/src/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+	String surname;
+	String name;
+	String position;
+	String email;
+	String phoneNumber;
+	int salary;
+	int age;
+	public Person(String surname, String name, String position, String email, String phoneNumber, int salary, int age)
+	{
+		this.surname = surname;
+		this.name = name;
+		this.position = position;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.salary = salary;
+		this.age = age;
+	}
+	void printInfo()
+	{
+		if (age > 40)
+		{
+			System.out.println("Фамилия сотрудника: " + this.surname);
+			System.out.println("Имя сотрудника: " + this.name);
+			System.out.println("Должность сотрудника: " + this.position);
+			System.out.println("Email сотрудника: " + this.email);
+			System.out.println("Телефон сотрудника: " + this.phoneNumber);
+			System.out.println("Зарплата сотрудника: " + this.salary);
+			System.out.println("Возраст сотрудника: " + this.age);
+		}
+	}
+}
